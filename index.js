4e710ed0018369113622346a15a8a9f97a2183d5
SadoninS <stanislavsadonin@gmail.com>
2023-06-14 21:47:30 +0400

    API and Logic

---
+++ index.js
@@ -0,0 +1,22 @@
+const favicon = document.querySelector('link[rel="shortcut icon"]');
+const bodyNode = document.querySelector('body');
+const headerNode = document.querySelector('#header');
+const textNode = document.querySelector('#text');
+const btnNode = document.querySelector('#btn');
+
+btnNode.addEventListener('click', () => {
+  fetch('http://www.boredapi.com/api/activity/')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      textNode.innerText = data.activity;
+      renderNewStyle();
+    });
+});
+
+function renderNewStyle() {
+  favicon.setAttribute('href', 'fun.png');
+  bodyNode.classList.add('fun');
+  headerNode.innerText = 'Have fun!🔥';
+}
